a3617db8ee7d6c398510b81dca861c7e8b48dcf7
Hrutam Sabale <168333028+hrutamsabale@users.noreply.github.com>
2024-07-23 22:15:20 +0530

    Update and rename myattempt-Day12-NumberGuess.py to Day12-NumberGuess.py

--- Day12-NumberGuess.py
+++ Day12-NumberGuess.py
@@ -16,8 +16,8 @@ def chose_number():
   print(f"\nYou've got {attempts} attempts")
   user_number=int(input("Enter a number: "))
   if user_number==chosen_number:
-    print("You Won!")
-    attempts=0
+    print(f"\nYou Won!. The answer was indeed {chosen_number}.")
+    attempts=-1
   elif user_number<chosen_number:
     print("Too Low")
     attempts-=1
@@ -27,4 +27,8 @@ def chose_number():
 while(attempts>0):
   chose_number()
   if attempts==0:
-    print("\nGame Over")
+    print(f"\nGame Over. The answer was {chosen_number}")
+  elif attempts==-1:
+    print("Game Over.")
+  else:
+    print("Guess Again")
