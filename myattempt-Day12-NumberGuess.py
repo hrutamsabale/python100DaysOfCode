7a3a3cf037a9ba1b46a47c5339fc9510e54b38a5
Hrutam Sabale <168333028+hrutamsabale@users.noreply.github.com>
2024-07-23 21:26:15 +0530

    Create myattempt-Day12-NumberGuess.py

---
+++ myattempt-Day12-NumberGuess.py
@@ -0,0 +1,31 @@
+import random
+chosen_number=random.randint(1,100)
+print(chosen_number)
+def difficulty():
+  difficulty=input("Choose a difficulty, 'Hard' or 'Easy': ").lower()
+  if difficulty=='easy':
+    attempts=10
+  elif difficulty=='hard':
+    attempts=5
+  else:
+    print("Invalid Choice")
+    attempts=0
+  return attempts
+attempts=difficulty()
+def chose_number():
+  global attempts
+  print(f"\nYou've got {attempts} attempts")
+  user_number=int(input("Enter a number: "))
+  if user_number==chosen_number:
+    print("You Won!")
+    attempts=0
+  elif user_number<chosen_number:
+    print("Too Low")
+    attempts-=1
+  else:
+    print("Too High")
+    attempts-=1
+while(attempts>0):
+  chose_number()
+  if attempts==0:
+    print("\nGame Over")
